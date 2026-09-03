4afda45728460b04e5b6811d11746a95aaeba83e
kazuyuk1 <artpashin07@gmail.com>
2026-09-03 09:12:28 +0300

    changed schema.sql in test

--- src/test/resources/schema.sql
+++ src/test/resources/schema.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS warehouse_history (
     color VARCHAR(50) NOT NULL,
     cotton_part INT NOT NULL CHECK (cotton_part BETWEEN 0 AND 100),
     quantity INT NOT NULL CHECK (quantity >= 0),
-    warehouse_id BIGINT UNIQUE NOT NULL CHECK (warehouse_id >= 0),
+    warehouse_id BIGINT NOT NULL CHECK (warehouse_id >= 0),
     date TIMESTAMP NOT NULL,
-    UNIQUE(color, cotton_part)
+    UNIQUE(event, color, cotton_part, quantity, date)
 );
